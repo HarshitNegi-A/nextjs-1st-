2b796bdc837f8a56685fc98f35a40fa646e90b6c
HarshitNegi-A <harshitnegi226@gmail.com>
2024-12-06 11:20:22 +0530

    aboutus

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function AboutUs(){
-    return <h1>About Us</h1>
+    const details = [
+
+        { id : 1, name: 'Yash', role: 'Senior Developer'},
+        
+        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+        
+        { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+        
+        ]
+    return <Fragment>
+        <h1>Developers</h1>
+        <ul>
+        {details.map((detail)=>(
+            <li><Link href={'/aboutus/'+detail.id}>{detail.name} </Link>  </li>
+        ))}
+        </ul>
+        </Fragment>
 }
 
 export default AboutUs;
